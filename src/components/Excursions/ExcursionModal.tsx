27b6cdbaadce30446f5c704830482e8f8c934d21
cold <kholodihor@gmail.com>
2024-04-02 11:24:49 +0300

    feat: prepare for deploy

---
+++ src/components/Excursions/ExcursionModal.tsx
@@ -0,0 +1,202 @@
+import people_icon from '@/assets/icons/people_icon.svg';
+import time_icon from '@/assets/icons/time_icon.svg';
+import close_icon_black from '@/assets/icons/close_icon_black.svg';
+import { closeModal, openModal } from '@/store/slices/modalSlice';
+import {
+  Dispatch,
+  MouseEvent,
+  SetStateAction,
+  useEffect,
+  useState
+} from 'react';
+import { Image } from '@/store/slices/gallerySlice';
+import { useAppDispatch, useAppSelector } from '@/store/hook';
+import { useTranslation } from 'react-i18next';
+import { Excursion } from '@/store/slices/excursionsSlice';
+import { createImgUrl } from '@/utils/createFileUrl';
+
+type ExcursionsModalProps = {
+  isOpen: boolean;
+  gallery: Image[];
+  setShowModal: Dispatch<SetStateAction<boolean>>;
+};
+
+const ExcursionModal = ({
+  isOpen,
+  gallery,
+  setShowModal
+}: ExcursionsModalProps) => {
+  const { language } = useTranslation().i18n;
+  const dispatch = useAppDispatch();
+  const excursion = useAppSelector((state) => state.modals.data) as Excursion;
+  const [imagesToDisplay, setImagesToDisplay] = useState<Image[]>([]);
+  const images = useAppSelector((state) => state.gallery.images);
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [windowHeight, setHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    const randomImages = [
+      gallery[generateRandom(gallery.length - 1)],
+      gallery[generateRandom(gallery.length - 1) + 1],
+      gallery[generateRandom(gallery.length - 1) + 1]
+    ];
+    setImagesToDisplay(randomImages);
+  }, [images]);
+
+  const handleChangedSize = () => {
+    setWindowWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleChangedSize);
+    return () => {
+      window.removeEventListener('resize', handleChangedSize);
+    };
+  }, []);
+
+  const closeExcursionsModal = () => {
+    setShowModal(false);
+    setTimeout(() => {
+      dispatch(closeModal());
+    }, 500);
+  };
+
+  const handleOverlayClick = (event: MouseEvent<HTMLDivElement>) => {
+    if (event.currentTarget === event.target) {
+      closeExcursionsModal();
+    }
+  };
+
+  const openExcursionOrderModal = () => {
+    setTimeout(() => {
+      dispatch(openModal({ data: {}, type: 'order' }));
+    }, 300);
+  };
+
+  const openDonationModal = () => {
+    dispatch(openModal({ data: {}, type: 'donation' }));
+  };
+
+  function generateRandom(max: number) {
+    let rand = Math.random() * max;
+    rand = Math.floor(rand);
+    return rand;
+  }
+
+  return (
+    <div
+      className={`fixed left-0 top-0 z-50 h-screen w-screen bg-black transition-all duration-700 ${
+        isOpen ? 'bg-opacity-40' : 'bg-opacity-0'
+      } `}
+      onClick={handleOverlayClick}
+    >
+      <div
+        className={`absolute left-1/2 top-1/2 ${
+          isOpen ? 'translate-x-0' : 'translate-x-[100%]'
+        } w-[85%] ${
+          windowHeight < 400 && 'md:h-[380px]'
+        } h-[675px] max-h-full translate-x-[-50%] translate-y-[-50%] overflow-auto bg-white 
+        px-5 pb-12 pt-4 transition-all duration-700 md:h-[832px] md:w-[672px] md:px-10 md:pb-10 md:pt-10 
+         lg:h-auto lg:w-[1136px] lg:px-[3.75rem] lg:pb-[3.75rem]`}
+      >
+        <div className="flex flex-col md:gap-6 lg:flex-row lg:gap-10">
+          <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-2 md:flex-row md:gap-6 lg:gap-9">
+              <div className="flex gap-3">
+                <img src={time_icon} width={24} height={24}></img>
+                <span className="text-sm leading-normal text-darkyellow md:text-base">
+                  {excursion.time_from} - {excursion.time_to}{' '}
+                  {language === 'uk' ? 'хвилин' : 'minutes'}
+                </span>
+              </div>
+              <div className="flex gap-3">
+                <img src={people_icon} width={24} height={24}></img>
+                <span className="text-sm leading-normal text-darkyellow md:text-base">
+                  {language === 'uk' ? 'до' : 'to'}{' '}
+                  {excursion.amount_of_persons}{' '}
+                  {language === 'uk' ? 'відвідувачів' : 'visitors'}
+                </span>
+              </div>
+            </div>
+            {windowWidth >= 1280 && (
+              <div className="grid grid-cols-2 gap-3.5">
+                {imagesToDisplay &&
+                  Array.isArray(imagesToDisplay) &&
+                  imagesToDisplay?.map((item: any, index: number) => (
+                    <div
+                      key={index}
+                      className={`group relative cursor-pointer  ${
+                        index === 2
+                          ? 'col-span-2 h-[260px] w-[490px]'
+                          : 'h-[260px] w-[238px]'
+                      }`}
+                    >
+                      <img
+                        src={createImgUrl(item?.image_url)}
+                        alt="cow"
+                        className="h-full w-full object-cover"
+                      />
+                    </div>
+                  ))}
+              </div>
+            )}
+            {windowWidth >= 768 && windowWidth < 1280 && (
+              <div className="flex gap-3">
+                <img
+                  src={createImgUrl(imagesToDisplay[0]?.image_url)}
+                  className="h-[194px] w-[290px] object-cover"
+                ></img>
+                <img
+                  src={createImgUrl(imagesToDisplay[1]?.image_url)}
+                  className="h-[194px] w-[290px] object-cover"
+                ></img>
+              </div>
+            )}
+            {windowWidth < 768 && (
+              <img
+                src={createImgUrl(imagesToDisplay[0]?.image_url)}
+                className="min-h-[186px] min-w-[280px] object-cover"
+              ></img>
+            )}
+          </div>
+          <div className="mt-[1.25rem] flex w-[90%] flex-col justify-between gap-3.5 md:mt-0 md:w-[592px] md:gap-6 lg:w-[28.75rem]">
+            <h3 className="pb-[7px] text-lg font-semibold leading-normal text-black md:mt-0 md:text-xl lg:border-b lg:border-disabled lg:text-2xl lg:font-bold">
+              {language === 'uk' ? excursion.title_ua : excursion.title_en}
+            </h3>
+            <p className="whitespace-pre-line text-sm font-normal leading-normal text-black md:text-base">
+              {language === 'uk'
+                ? excursion.description_ua
+                : excursion.description_en}
+            </p>
+            <div className="flex flex-col gap-3 md:flex-row md:justify-center md:gap-6">
+              <button
+                className="h-10 max-w-[17.5rem] bg-accent text-lg font-medium leading-[1.375rem] transition-all duration-300 hover:bg-lemon focus:bg-lemon active:bg-lemon md:h-11 md:w-[14.44rem]"
+                onClick={openExcursionOrderModal}
+              >
+                {language === 'uk'
+                  ? 'Замовити екскурсію'
+                  : 'Order an excursion'}
+              </button>
+              <button
+                className="h-10 max-w-[17.5rem] border border-solid border-black text-lg font-medium leading-[1.375rem] text-black hover:border-accent focus:border-accent active:border-accent md:h-11 md:w-[14.44rem]"
+                onClick={openDonationModal}
+              >
+                {language === 'uk' ? 'Допомогти' : 'Donate'}
+              </button>
+            </div>
+          </div>
+        </div>
+        <button
+          className="absolute right-6 top-6"
+          onClick={() => closeExcursionsModal()}
+        >
+          <img src={close_icon_black} width={44} height={44}></img>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ExcursionModal;
---
+++ src/components/Excursions/Excursions.tsx
@@ -0,0 +1,232 @@
+import { useEffect, useState } from 'react';
+import { useAppDispatch, useAppSelector } from '@/store/hook';
+import { openModal } from '@/store/slices/modalSlice';
+import { useTranslation } from 'react-i18next';
+import { useInView } from 'react-intersection-observer';
+
+import ExcursionsReviews from './ExcursionsReviews';
+import ExcursionModal from './ExcursionModal';
+import LittleArrow from '../icons/LittleArrow';
+import Slider from '@/components/Slider';
+import ExcursionOrderModal from './ExcursionOrderModal';
+import { setActiveLink } from '@/store/slices/observationSlice';
+import {
+  Excursion,
+  fetchExcursionsWithPagination
+} from '@/store/slices/excursionsSlice';
+import { useWidth } from '@/hooks/useWidth';
+import { fetchImages } from '@/store/slices/gallerySlice';
+import { createImgUrl } from '@/utils/createFileUrl';
+
+const Excursions = () => {
+  const { language } = useTranslation().i18n;
+  const dispatch = useAppDispatch();
+  const screenWidth = useWidth();
+  const [showModal, setShowModal] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(6);
+  const [pagesLength, setPagesLength] = useState(0);
+  const type = useAppSelector((state) => state.modals.type);
+  const isModalOpen = useAppSelector((state) => state.modals.isModalOpen);
+  const gallery = useAppSelector((state) => state.gallery.images);
+
+  useEffect(() => {
+    dispatch(fetchImages());
+  }, []);
+
+  const { excursions, totalLength } = useAppSelector(
+    (state) => state.excursions.paginatedData
+  );
+  const { ref, inView } = useInView({
+    threshold: 0.5
+  });
+
+  useEffect(() => {
+    if (screenWidth >= 1280) {
+      setItemsPerPage(3);
+    }
+    if (screenWidth >= 768 && screenWidth < 1280) {
+      setItemsPerPage(2);
+    }
+    if (screenWidth > 320 && screenWidth < 768) {
+      setItemsPerPage(totalLength);
+    }
+  }, [screenWidth, totalLength]);
+
+  useEffect(() => {
+    const pagesNumber = totalLength / itemsPerPage;
+    setPagesLength(pagesNumber < 5 ? pagesNumber : 5);
+  }, [totalLength, itemsPerPage]);
+
+  useEffect(() => {
+    dispatch(
+      fetchExcursionsWithPagination({ page: currentPage, limit: itemsPerPage })
+    )
+      .unwrap()
+      .then(() => {
+        return [];
+      })
+      .catch((error) => alert(error));
+  }, [currentPage, dispatch, itemsPerPage]);
+
+  const openExcursionModal = (item: Excursion) => {
+    dispatch(openModal({ data: item, type: 'excursions' }));
+  };
+
+  useEffect(() => {
+    if (isModalOpen) {
+      setShowModal(true);
+    } else {
+      setShowModal(false);
+    }
+  }, [isModalOpen]);
+
+  useEffect(() => {
+    if (inView) {
+      dispatch(setActiveLink('#excursions'));
+    } else {
+      dispatch(setActiveLink(''));
+    }
+  }, [inView, dispatch]);
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleChangedSize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleChangedSize);
+    return () => {
+      window.removeEventListener('resize', handleChangedSize);
+    };
+  }, []);
+
+  return (
+    <section
+      id="excursions"
+      ref={ref}
+      className="relative bg-[#F3F3F5]  py-6  md:py-16 lg:py-20"
+    >
+      <div className="mx-auto px-5 sm:w-[480px] md:w-[768px] md:px-12 lg:w-[1280px] lg:px-[120px] xl:w-[1440px]">
+        {windowWidth < 768 && (
+          <div>
+            <h2 className="mb-10 flex gap-2 text-2xl font-medium leading-normal">
+              {language === 'uk' ? 'Екскурсії' : 'Excursions'}
+              <img src="/cow.svg" alt="" className="" />
+            </h2>
+            <ul className="mb-5 flex flex-col items-center gap-4">
+              {excursions && Array.isArray(excursions) ? (
+                excursions.map((excursion: Excursion) => (
+                  <li key={excursion.id} className="drop-shadow">
+                    <div className="relative">
+                      <img
+                        src={createImgUrl(excursion.image_url)}
+                        alt={`Excursion Image`}
+                        width={280}
+                        height={200}
+                      ></img>
+                      <div className="absolute bottom-0 left-0 flex flex-col gap-3 pb-5 pl-5 text-white">
+                        <p className="text-lg font-medium leading-normal">
+                          {language === 'uk'
+                            ? excursion.title_ua
+                            : excursion.title_en}
+                        </p>
+                        <a>
+                          <button
+                            className="flex gap-3 border border-solid border-white py-2.5 pl-5 pr-4"
+                            onClick={() => {
+                              openExcursionModal(excursion);
+                            }}
+                          >
+                            <span className="text-base leading-tight">
+                              {language === 'uk'
+                                ? 'Показати більше'
+                                : 'Show more'}
+                            </span>
+                            <LittleArrow />
+                          </button>
+                        </a>
+                      </div>
+                    </div>
+                  </li>
+                ))
+              ) : (
+                <p className="text-black">Сервер не відповідає</p>
+              )}
+            </ul>
+          </div>
+        )}
+        {windowWidth >= 768 && (
+          <Slider
+            title={language === 'uk' ? 'Екскурсії' : 'Excursions'}
+            setCurrentPage={setCurrentPage}
+            pagesLength={pagesLength}
+            isExcursions={true}
+          >
+            <ul className="mb-0 flex justify-center gap-6 lg:mb-[8.75rem]">
+              {excursions && Array.isArray(excursions) ? (
+                excursions.map((excursion: Excursion) => (
+                  <li key={excursion.id} className="drop-shadow">
+                    <div className="group relative">
+                      <img
+                        className="h-[235px] w-[324px] lg:h-[384px] lg:w-[384px]"
+                        src={createImgUrl(excursion.image_url)}
+                        alt={`Excursion Image`}
+                      ></img>
+                      <div className="absolute left-0 top-0 h-full w-full lg:bg-black/[.60] lg:opacity-0 lg:transition-all lg:duration-700 lg:group-hover:opacity-100"></div>
+                      <div className="absolute bottom-0 left-0 flex flex-col gap-0 pb-6 pl-6 text-white transition-all duration-700 group-hover:gap-5">
+                        <p className="text-xl font-semibold leading-normal lg:text-[22px]">
+                          {language === 'uk'
+                            ? excursion.title_ua
+                            : excursion.title_en}
+                        </p>
+                        <p className="opacity-0 transition-all duration-700 lg:group-hover:opacity-100">
+                          {excursion.time_from} - {excursion.time_to}{' '}
+                          {language === 'uk' ? 'хвилин' : 'minutes'} /{' '}
+                          {language === 'uk' ? 'до' : 'up to'}{' '}
+                          {excursion.amount_of_persons}{' '}
+                          {language === 'uk' ? 'відвідувачів' : 'visitors'}
+                        </p>
+                        <a>
+                          <button
+                            className="flex gap-3 border border-solid border-white py-[0.69rem] pl-6 pr-2.5 transition-all duration-700 lg:border-transparent lg:group-hover:border-white lg:group-hover:hover:border-accent lg:focus:border-accent lg:active:border-accent"
+                            onClick={() => {
+                              openExcursionModal(excursion);
+                            }}
+                          >
+                            <span className="text-lg font-medium leading-tight">
+                              {language === 'uk'
+                                ? 'Показати більше'
+                                : 'Show more'}
+                            </span>
+                            <LittleArrow />
+                          </button>
+                        </a>
+                      </div>
+                    </div>
+                  </li>
+                ))
+              ) : (
+                <p className="text-black">Сервер не відповідає</p>
+              )}
+            </ul>
+          </Slider>
+        )}
+        <ExcursionsReviews />
+        {isModalOpen && type === 'excursions' && (
+          <ExcursionModal
+            isOpen={showModal}
+            setShowModal={setShowModal}
+            gallery={gallery}
+          />
+        )}
+        {isModalOpen && type === 'order' && (
+          <ExcursionOrderModal isOpen={showModal} setShowModal={setShowModal} />
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Excursions;
